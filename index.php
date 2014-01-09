fe87d3df70e7d66daf40efcdca9881d2764d4291
GrCOTE7 <GrCOTE7@GMail.com>
2014-01-09 09:39:49 -0800

    maj

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 **                                                                            **
 ** We're looking for contributors for developp this Pomodoro 's Project       **
 **                                                                            **
-** You work with NetBeans, know HTML5 / CSS3 or PHP or MySQL or jQuery ?      **
+** You can work with NetBeans, know HTML5 / CSS3 or PHP or MySQL or jQuery ?  **
 **                                                                            **
 ** Please contact-us at : POMODO dot MANUDON.COM                              **
 **                                                                            **
